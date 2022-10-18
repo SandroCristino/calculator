9564ae6ffcad99c73fdaf178b84b49e454798358
SandyWezzy <sandro-cristino@hotmail.de>
2022-10-18 21:52:52 +0200

    build design

---
+++ script.js
@@ -0,0 +1,5 @@
+const resultOutput = document.querySelector('.display');
+const deleteBtn = document.querySelector('.deleteBtn');
+const clearBtn = document.querySelector('.clearBtn');
+const valueBtn = document.querySelectorAll('#downerBtn');
+
