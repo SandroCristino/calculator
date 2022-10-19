f956e95341e15fdbb8f555ffc1649f2c0886365c
SandyWezzy <sandro-cristino@hotmail.de>
2022-10-19 16:26:53 +0200

    build verion one

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const endResult = document.querySelector('.endResult');
 const interResult = document.querySelector('.interResult');
 const everyBtn = document.querySelectorAll('#Btn');
+const deleteBtn = document.querySelector('.deleteBtn');
+const clearBtn = document.querySelector('.clearBtn');
+
 
 let operator = '';
 let currentInput = '0';
@@ -8,6 +11,7 @@ let firstInputSave = '';
 let secondInputSave = '';
 let constantResult = '';
 let result = 0;
+let allowNumberCheck = false;
 
 const add = (x,y) => x+y;
 const sub = (x,y) => x-y;
@@ -19,36 +23,63 @@ const divi = (x,y) => x/y;
 
 
 everyBtn.forEach((button) => button.addEventListener('click', () => pressedBtn(button.textContent)));
-
+    
 function pressedBtn(btn) {
+    
+
     switch (btn) {
-        case '1': case'2': case '3': case '4': case '5': case '6': case '7': case '8': case'9': case '0':
-            currentInput = btn;
+        case '1': case'2': case '3': case '4': case '5': case '6': case '7': case '8': case'9': case '0': case '.':
+            if (!allowNumberCheck) {
+                
+                currentInput = btn;
+                
+            
+                if (currentInput == '') {
+                    firstInputSave = currentInput;
+                } 
 
-            if (currentInput == '0'){
-                currentInput == '';
-            }
+                // if (firstInputSave != '') {
+                    
+                // }
 
-            if (operator == '' ){
-                firstInputSave += currentInput;
-                endResult.textContent = firstInputSave;
-            } else {
-                secondInputSave += currentInput;
-                endResult.textContent = secondInputSave;
-            }
-
-            currentInput = '';
+                if (operator == ''){
+                    firstInputSave += currentInput;
+                    endResult.textContent = firstInputSave;
+                } else {
+                    secondInputSave += currentInput;
+                    endResult.textContent = secondInputSave;
+                }
+            }   
 
             break;
 
         case '+': case '-': case '/': case 'x':
             operate();
             operator = btn;
+            if (firstInputSave == '') {
+                firstInputSave = currentInput;
+            }
             currentInput = 0;
             interResult.textContent = endResult.textContent + ' ' + operator;
+            allowNumberCheck = false;
             break;
         case '=':
             operate();
+            break;
+        case 'Clear':
+            allowNumberCheck = false;
+            firstInputSave = '', secondInputSave = '', operator = '', currentInput = '0';
+            interResult.textContent = '';
+            endResult.textContent = '0';
+            break;
+        case 'Delete':
+            if (operator == '' ){
+                firstInputSave = '';
+                endResult.textContent = '0';
+            } else {
+                secondInputSave = '';
+                endResult.textContent = '0';
+            }
             break;
         default:
             console.log('Error');
@@ -56,8 +87,8 @@ function pressedBtn(btn) {
 
     function operate() {
         if (firstInputSave != '' && secondInputSave != '' && operator != '') {
-            let first = parseInt(firstInputSave);
-            let second = parseInt(secondInputSave);
+            let first = parseFloat(firstInputSave);
+            let second = parseFloat(secondInputSave);
             switch (operator) {
                 case '+':
                     result = add(first,second);
@@ -77,17 +108,32 @@ function pressedBtn(btn) {
             interResult.textContent = result;
             endResult.textContent = result;
             operator = '';
-            firstInputSave = result;
+            currentInput = result;
+            firstInputSave = '';
             secondInputSave = '';
-
+            allowNumberCheck = true;
 
 
             console.log(first)
             console.log(second)
             console.log(result);
+            console.log(firstInputSave)
         };
     }
+function getCurrentVar () {
+    if (secondInputSave == '' && operate == '') {
+        return secondInputSave;
+    } else {
+        return firstInputSave;
+    }
 }
+
+
+
+        
+}
+    
+
 
 
 
